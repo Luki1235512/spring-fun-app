ff046ea12489e1ad0dcf2164c75baa145bfaed29
Luki1235512 <Luki1235512@gmail.com>
2022-04-07 12:11:27 +0200

    created signup form

--- src/main/java/com/example/springfunapp/controllers/AppController.java
+++ src/main/java/com/example/springfunapp/controllers/AppController.java
@@ -1,8 +1,10 @@
 package com.example.springfunapp.controllers;
 
+import com.example.springfunapp.models.User;
 import com.example.springfunapp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,6 +16,13 @@ public class AppController {
     @GetMapping("")
     public String viewHomePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
     }
 
 }
