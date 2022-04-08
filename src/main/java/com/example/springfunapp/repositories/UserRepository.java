943e8310afd89cad224a1ccb13d1ff5b283dcc57
Luki1235512 <Luki1235512@gmail.com>
2022-04-08 17:07:59 +0200

    configured spring security

--- src/main/java/com/example/springfunapp/repositories/UserRepository.java
+++ src/main/java/com/example/springfunapp/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.springfunapp.repositories;
 
 import com.example.springfunapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
 
 }
