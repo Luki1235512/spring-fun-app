c03cb61a9a342a0e96fabd4b80c0dfa4bea4af83
Luki1235512 <Luki1235512@gmail.com>
2022-04-08 16:17:59 +0200

    added registration

--- src/main/java/com/example/springfunapp/controllers/AppController.java
+++ src/main/java/com/example/springfunapp/controllers/AppController.java
@@ -3,9 +3,11 @@ package com.example.springfunapp.controllers;
 import com.example.springfunapp.models.User;
 import com.example.springfunapp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -23,6 +25,17 @@ public class AppController {
         model.addAttribute("user", new User());
 
         return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "register_success";
     }
 
 }
